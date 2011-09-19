607adf9bd1c17e04fc6dd11818a3fc81f54b815c
tmandersson <ltmandersson@gmail.com>
2011-09-19 21:14:37 +0200

    Next limit is the LS size limit.

--- src/spu_test/spu_test_main.c
+++ src/spu_test/spu_test_main.c
@@ -90,8 +90,8 @@ int main(int argc, char* argv[]) {
 	print_values(spu_result2);
 
 	printf("\n\nSPU CODE with bigger fractal:\n");
-	int height = 65;
-	int width = 65;
+	int height = 250;
+	int width = 250;
 	x_step = (max_re - min_re) / width;
 	y_step = (max_im - min_im) / height;
 	params.pixel_width = width;
