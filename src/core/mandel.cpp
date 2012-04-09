728cd517a89ad82af1bdb5c26580ef57ea23eaa7
tmandersson <ltmandersson@gmail.com>
2012-04-09 22:21:45 +0200

    Alright now SPU's is used. Old threading code is removed. A ton more clean up should be done. But I might have lost interest at this point. It was fun to play around with this from time to time as exercise. But it might be time I move on to something that has higher value...
    Anyone looking at my code should take it for what it is.
    Hackfest/learning/duct-tape code. Whole thing can be simplified
    to a lot fewer moving objects. Pointless design is left from what
    I used as a starting point. (Old highschool code.)
    
    Was cool to see that it was not impossible to get those SPU's
    working and getting hot though. :)

--- src/core/mandel.cpp
+++ src/core/mandel.cpp
@@ -1,43 +1,13 @@
 #include "core/mandel.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include "core/mftb_profiling.h"
-#include <unistd.h>
-#include <sys/thread.h>
 
 #include <core/fractal_params.h>
 #include <sys/spu.h>
 #include "spu_bin.h"
 #define ptr2ea(x)			((u64)((void*)(x)))
 
-const int CALCULATION_THREADS = 5;
-
-typedef sys_ppu_thread_t pthread_t;
-
-int pthread_create (pthread_t *id, void *attr, void *(* __start_routine) (void *), void * __arg)
-{
-	void (* routine) (void *) = (void (*)(void *)) __start_routine;
-	return sysThreadCreate(id, routine, __arg, 1001, 0x100000, THREAD_JOINABLE, NULL);
-}
-
-void pthread_exit (void *__retval) {
-	sysThreadExit(0);
-}
-
-int pthread_join (pthread_t __th, void **__thread_return) {
-	u64 ret_val;
-	return sysThreadJoin(__th, &ret_val);
-}
-
 const unsigned int ITERATIONS = 256;
-
-struct section_params {
-	int start_pixel_row;
-	int end_pixel_row;
-	double start_im;
-	Mandel *mandel_object;
-};
 
 struct thread_data {
 	u32 id;
@@ -93,13 +63,6 @@ Mandel::Mandel(int width, int height, IPlotter &plotter) : _plotter(plotter)
 	_max_iterations = ITERATIONS;
 	_old_min_re = _old_max_re = _old_min_im = _old_max_im = 0;
 	_have_painted = false; // haven't drawn the fractal yet
-}
-
-void *call_calculate_section(void *params)  {
-	Mandel *obj = ((section_params *)params)->mandel_object;
-	obj->calculate_section(params);
-	pthread_exit(NULL);
-	return NULL;
 }
 
 void Mandel::paint()
@@ -187,79 +150,4 @@ void Mandel::zoom_back()
 		_max_im = _old_max_im;
 		zoom(_min_re, _max_re, _min_im, _max_im);
 	}
-}
-
-void Mandel::calculate_section(void *params) {
-	section_params *p;
-	p = (section_params *) params;
-	int start_pixel_row = p->start_pixel_row;
-	int end_pixel_row = p->end_pixel_row;
-	double start_im = p->start_im;
-
-	int y;
-	double re;
-	double im = start_im;
-	for (y = start_pixel_row; y < end_pixel_row; y++) {
-		if (y > start_pixel_row) {
-			im -= _y_step;
-		}
-		re = _min_re; // start with the first pixel on the row
-		calculate_row(re, im, y);
-	}
-}
-
-void Mandel::calculate_row(double re, double im, int y) {
-	int x;
-	for (x = 0; x < _width; x++) {
-		if (x > 0)
-			re += _x_step;
-
-		unsigned int iterations = calculate(re, im);
-
-		if ( iterations != 0)
-			_plotter.plot(x, y, (iterations % 255) + 1);
-		else
-			_plotter.plot(x, y, 0);
-	}
-}
-
-// Iterate Zn+1 = Zn^2 + C till we now if the iterations is gonna reach infinity
-// or not.
-// If we don't reach infinity then the function returns the number of 
-// iterations that were needed and the complex constant C belongs to the 
-// Mandel set.
-// We start with n = 0 (Z0 = 0)
-// C is the first argument (it also stands for wich pixel on the screen that 
-// we're calculating)
-// the second argument is the maximal number of iterations before we consider
-// C a part of the Mandel set.
-unsigned int Mandel::calculate(double c_re, double c_im)
-{
-	double z_re, z_im;
-	z_re = z_im = 0;
-	unsigned iterations;
-	bool infinity = false;
-	
-	// Stop when we maximum number of iterations is reached (part of Mandel set)
-	// or when we're certain that the iteration is going to reach infinity.
-	for (iterations = 0; iterations < _max_iterations && !infinity; iterations++) {
-		// z = z*z + c;
-		double new_z_re, new_z_im;
-		new_z_re = ((z_re*z_re) - (z_im*z_im) + c_re);
-		new_z_im = ((z_re*z_im * 2) + c_im);
-		z_re = new_z_re;
-		z_im = new_z_im;
-
-		// We now that everything outside a circle with the radius of
-		// 2 is outside the Mandel set.
-		// Thus if the abs(z) > 2 then the iterations is going to reach infinity
-		if ((z_re*z_re + z_im*z_im) > 4)
-			infinity = true;
-	}
-	
-	// if the iteration don't reach infinity then C is part of the	Mandel set
-	if (!infinity)
-		return 0;
-	else
-		return iterations;
 }
